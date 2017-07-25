5338ca9ffb239eac91d45b9c2b4b520815931e8f
StasBel <stasbelyaev96@gmail.com>
2017-07-25 18:11:23 +0300

    Implement sklearn baseline clustering

--- new_main.py
+++ new_main.py
@@ -1,11 +1,24 @@
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import Pipeline
+from sklearn.cluster import AffinityPropagation, KMeans, SpectralClustering, DBSCAN, MiniBatchKMeans, MeanShift
+from sklearn.metrics import calinski_harabaz_score, silhouette_score
+from sklearn.mixture import BayesianGaussianMixture
 
 from new_pipe.code_gens import *
 from new_pipe.cookers import *
 from new_pipe.preprocessor import Preprocessor
 
 if __name__ == '__main__':
+    X = np.array(list(from_csv("data/step-12768-submissions.csv", nrows=1000)))
     pipeline = Pipeline([("pre", Preprocessor(language="python", method="tokenize")),
-                         ("bow", BagOfWords(ngram_range=(1, 3)))])
-    print(pipeline.fit_transform(list(from_csv("data/step-12768-submissions.csv", nrows=100))).shape)
-    print(pipeline.named_steps["bow"].vocabulary_)
+                         ("bot", BagOfWords()),
+                         ("idf", TfidfTransformer())])
+    vecs = pipeline.fit_transform(X)
+    # clu = DBSCAN(min_samples=5, n_jobs=-1)
+    clu = MiniBatchKMeans(n_clusters=10)
+    # clu = MeanShift(n_jobs=-1)
+    labels = clu.fit_predict(vecs)
+    # print(labels.max() + sum(labels == -1))
+    print(labels)
+    X = X[pipeline.named_steps["pre"].correct_ind]
+    print(calinski_harabaz_score(vecs.toarray(), labels))
--- new_pipe/preprocessor.py
+++ new_pipe/preprocessor.py
@@ -20,13 +20,19 @@ class Preprocessor(BaseEstimator, TransformerMixin):
         self.unk_str = unk_str
 
         self.encoding = DefaultIntBijection(zero_value=unk_str if add_unk else None)
+        self.correct_ind = []
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
         language = languages[self.language]
-        X = filter(language.check, X) if self.filter_correct else X
+        nX = []
+        for i, x in enumerate(X):
+            if not self.filter_correct or (self.filter_correct and language.check(x)):
+                self.correct_ind.append(i)
+                nX.append(x)
+        X = nX
         X = map(language[self.method], X)
         X = map(lambda x: encode(x, self.encoding), X)
         return list(X)

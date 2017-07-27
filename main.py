94f67cf678e8d815ec596483df821bd8d9e94e65
StasBel <stasbelyaev96@gmail.com>
2017-07-27 03:21:21 +0300

    Delete old pipe implementation

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from models.node_embedding import *
+from difflib import SequenceMatcher
+
+from sklearn.cluster import DBSCAN
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+
+from models.utils.preprocessing import telegram_send
+from pipe.code_gens import *
 from pipe.cookers import *
-from pipe.initers import *
-from pipe.pickler import *
-from pipe.pipe_base import *
-from pipe.preprocessor import *
-from pipe.terminaters import *
+from pipe.plot import plot
+from pipe.preprocessor import Preprocessor
 
-
+"""
 def train_vec(node_embedding):
     pc_embedding = ParentChildrenEmbedding(epochs=10, N=10000000, variables_dump="data/model.ckpt")
     node_vec = pc_embedding.fit(node_embedding.dataset)
@@ -24,11 +28,60 @@ def do_if_not_exists(path):
         pipe.transform(ForNodeEmbedding())
         node_embedding = pipe.terminate(Pickle(path))
     train_vec(node_embedding)
+"""
 
 
-def main():
-    do_if_not_exists("data/node_embedding.dump")
+def make_pipe():
+    X = pd.read_csv("data/step-12768-submissions.csv", nrows=1000)
+    pipeline = Pipeline([("pre", Preprocessor(language="python", method="astize")),
+                         ("bot", BagOfTrees(ngram_range=(1, 2))),
+                         ("idf", TfidfTransformer())])
+    vecs = pipeline.fit_transform(X["code"]).toarray()
+    X = X.iloc[pipeline.named_steps["pre"].correct_ind]
+    # clu = MiniBatchKMeans(n_clusters=10)
+    clu = DBSCAN(eps=0.25, min_samples=2, n_jobs=-1)
+    # clu = AffinityPropagation(damping=.95)
+    labels = clu.fit_predict(vecs)
+
+    plot(vecs, labels,
+         method="pca",
+         code=X["code"],
+         correct=(X["status"] == "correct").astype(int),
+         # scaling="centers",
+         # centres=clu.cluster_centers_,
+         title="Submissions of step №12768 clustering")
+
+    """
+    # X = np.array(list(from_csv("data/step-12768-submissions.csv", nrows=1000)))
+    # clu = DBSCAN(min_samples=5, n_jobs=-1)
+    clu = MiniBatchKMeans(n_clusters=10)
+    # clu = MeanShift(n_jobs=-1)
+    labels = clu.fit_predict(vecs)
+    # print(labels.max() + sum(labels == -1))
+    print(labels)
+    X = X[pipeline.named_steps["pre"].correct_ind]
+    print(calinski_harabaz_score(vecs.toarray(), labels))
+    """
+
+
+def make_dataset_with_ratio():
+    df = pd.read_csv("data/step-12768-submissions.csv")
+    correct_code = df[df.status == "correct"]["code"]
+    step = -1
+
+    def apply_f(c):
+        nonlocal step
+        step += 1
+        if step % 100 == 0:
+            telegram_send("step= {}".format(step))
+        if c[1] != "correct":
+            return max(map(lambda cc: SequenceMatcher(None, str(c[0]), cc).ratio(), correct_code))
+        else:
+            return 1.
+
+    df["best_ratio"] = pd.Series(df[["code", "status"]].apply(apply_f, axis=1))
+    df.to_csv("data/step-12768-submissions-with-ratio.csv")
 
 
 if __name__ == '__main__':
-    main()
+    make_pipe()
--- pipe/preprocessor.py
+++ pipe/preprocessor.py
@@ -1,22 +1,38 @@
-from .languages import LANGUAGES
-from .pipe_base import Transformer
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from .languages import languages
 from .primitives import DefaultIntBijection
 
 
-class Preprocessor(Transformer):
-    UNK_STR = "<UNK>"
+def encode(struct, encoding):
+    if isinstance(struct, list):
+        return [encoding[elem] for elem in struct]
+    else:
+        return struct.map(encoding)
 
-    def __init__(self, language, method, filter_correct=True, add_unk=True):
+
+class Preprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self, language, method, filter_correct=True, add_unk=True, unk_str="<UNK>"):
         self.language = language
         self.method = method
         self.filter_correct = filter_correct
         self.add_unk = add_unk
+        self.unk_str = unk_str
 
-    def transform(self, gen, state):
-        language = LANGUAGES[self.language]
-        check = language["check"]
-        method = language[self.method]
-        gen = filter(check, gen) if self.filter_correct else gen
-        encoding = DefaultIntBijection(zero_value=self.UNK_STR if self.add_unk else None)
-        state.encoding = encoding
-        return map(lambda x: method(x, encoding), gen)
+        self.encoding = DefaultIntBijection(zero_value=unk_str if add_unk else None)
+        self.correct_ind = []
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        language = languages[self.language]
+        nX = []
+        for i, x in enumerate(X):
+            if not self.filter_correct or (self.filter_correct and language.check(x)):
+                self.correct_ind.append(i)
+                nX.append(x)
+        X = nX
+        X = map(language[self.method], X)
+        X = map(lambda x: encode(x, self.encoding), X)
+        return list(X)

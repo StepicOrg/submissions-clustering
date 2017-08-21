f22924141a8a63b23a67b0202734d8463245b9a0
StasBel <stasbelyaev96@gmail.com>
2017-08-21 22:07:16 +0300

    Add seaborn and mpl deps, also example of calc error score

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import subsclu
+from subsclu import scorers
 from subsclu.utils import read as read_utils
 
 
 def main():
-    model = subsclu.from_spec("python", "diff")
+    model = subsclu.from_spec("python", "test")
     submissions = read_utils.from_sl3("data/subs.sl3", nrows=1000)
     submissions = list(read_utils.filter_out_invalid(submissions, "python"))
     model.fit(submissions)
-    print(model.neighbors([submissions[0][0]])[0].shape)
+
+    scorer = scorers.from_spec("python", "diff")
+    score = scorer.score(
+        model, submissions,
+        presaved_path="data/python_diff_1000.dump"
+    )
+    print(score)
 
 
 if __name__ == '__main__':

a85ea7abe5d9d6d71270cd29b1a649c103d2c655
StasBel <stasbelyaev96@gmail.com>
2017-08-22 13:43:47 +0300

    Make version getter private

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ for requirement in parse_requirements("requirements.txt", session="hack"):
         _dependency_links.append(str(requirement.link))
 
 
-def myversion():
+def _use_scm_version():
     from setuptools_scm.version import get_local_dirty_tag
 
     def clean_scheme(version):
@@ -43,7 +43,7 @@ setup(
     install_requires=_install_requires,
     dependency_links=_dependency_links,
     python_requires=">=3.4, <4",
-    use_scm_version=myversion,
+    use_scm_version=_use_scm_version,
     setup_requires=["setuptools_scm", "pytest-runner"],
     tests_require=["pytest"]
 )
